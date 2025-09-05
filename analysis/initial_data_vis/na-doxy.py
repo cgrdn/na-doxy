3e581d7372463dbddf5323b3b7ddbe9eed01a2fd
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2025-09-05 12:00:09 -0300

    final script

--- analysis/initial_data_vis/na-doxy.py
+++ analysis/initial_data_vis/na-doxy.py
@@ -8,20 +8,27 @@ import seaborn as sns
 
 import argopy
 
-# specify parameter
-param = 'DOXY'
-
 # load BGC index
 ix = argopy.ArgoIndex(index_file='bgc-s').load()
 
-for region in [list(Path('../../meta/regions').glob('*.csv'))[1]]:
+# select parameter, get some metadata info
+param = 'DOXY'
+reftbl = argopy.ArgoNVSReferenceTables().tbl('R03')
+param_info = reftbl[reftbl['altLabel']==param].iloc[0]
+
+# whether to fetch new data or grab from a previous run
+fresh = True
+
+
+
+for region in Path('../../meta/regions').glob('scotian*.csv'):
     
     coords = pd.read_csv(region)
     box = [coords.longitude.min(), coords.longitude.max(), coords.latitude.min(), coords.latitude.max(), 0, 9999]
 
     # extract box first then polygon
     df = ix.query.compose({
-        'box':[coords.longitude.min(), coords.longitude.max(), coords.latitude.min(), coords.latitude.max(), '1995-01-01', '2026-01-01'],
+        'box':[coords.longitude.min(), coords.longitude.max(), coords.latitude.min(), coords.latitude.max(), '2020-01-01', '2026-01-01'],
         'params':param,
     }).to_dataframe()
 
@@ -31,11 +38,35 @@ for region in [list(Path('../../meta/regions').glob('*.csv'))[1]]:
 
         sys.stdout.write(f'loading {cycles.shape[0]} cycles for WMO {wmo} ({i+1}/{df.wmo.unique().shape[0]})...')
 
-        data = argopy.DataFetcher(mode='expert', ds='bgc', params='DOXY').profile(wmo, cycles)
+        data = argopy.DataFetcher(mode='expert', ds='bgc', params=param).profile(wmo, cycles)
         profiles = pd.concat((profiles, data.to_dataframe()))
 
         sys.stdout.write('done\n')
+    
+    profiles = profiles.reset_index()
 
-        if i > 4:
-            break
-        
+    fig, ax = plt.subplots()
+    g = sns.lineplot(
+        data=profiles.loc[profiles.DOXY_QC < 4], x='DOXY', y='PRES', 
+        hue='PLATFORM_NUMBER', units='CYCLE_NUMBER', 
+        palette=sns.dark_palette('dodgerblue'), alpha=0.7,
+        orient='y', estimator=None, legend=False,
+        ax=ax
+    )
+    g.axes.set_ylim((2050, -50))
+
+    ax.set_title(f"Profiles of {param_info['prefLabel']} ({param})\n{ix.N_MATCH} profiles ({profiles.PLATFORM_NUMBER.unique().shape[0]} floats) from the {ix.convention_title}")
+    fig.savefig(f'figures/{region.name.replace(".csv", "")}_doxy_profiles.png', dpi=350, bbox_inches='tight')
+    plt.close(fig)
+
+    fig, axes = plt.subplots(3, 1, sharex=True)
+    surf = 50
+    surface = profiles.loc[profiles.PRES < surf]
+    sns.scatterplot(data=surface, x='TIME', y='TEMP', hue='PLATFORM_NUMBER', palette=sns.dark_palette('crimson'), alpha=0.7, legend=False, ax=axes[0])
+    sns.scatterplot(data=surface, x='TIME', y='PSAL', hue='PLATFORM_NUMBER', palette=sns.dark_palette('seagreen'), alpha=0.7, legend=False, ax=axes[1])
+    sns.scatterplot(data=surface, x='TIME', y='DOXY', hue='PLATFORM_NUMBER', palette=sns.dark_palette('dodgerblue'), alpha=0.7, legend=False, ax=axes[2])
+    plt.show()
+
+    axes[0].set_title(f"Timeseries for the top {surf}m of {param_info['prefLabel']} ({param})\n{ix.N_MATCH} profiles ({df.wmo.unique().shape[0]} floats) from the {ix.convention_title}")
+    fig.savefig(f'figures/{region.name.replace(".csv", "")}_timeseries.png', dpi=350, bbox_inches='tight')
+    plt.close(fig)
